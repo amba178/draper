e2b56be234e6c360b794cd7439470df4f315ed99
Salem Amba <salemamba@gmail.com>
2016-07-05 17:27:25 -0700

    start moving code from view to decorator class.

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -2,6 +2,11 @@ class UserDecorator < Draper::Decorator
   delegate_all 
   decorates_finders
 
+
+  def avatar 
+  	h.link_to_if model.url.present?, h.image_tag("avatars/#{avatar_name}", class: "avatar"), model.url
+  end
+
   # Define presentation-specific methods here. Helpers are accessed through
   # `helpers` (aka `h`). You can override attributes, for example:
   #
@@ -12,3 +17,12 @@ class UserDecorator < Draper::Decorator
   #   end
 
 end
+
+private
+	def avatar_name 
+	    if model.avatar_image_name.present?
+	      model.avatar_image_name
+	    else
+	      "default.png"
+	    end
+	end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,10 +1,4 @@
 module UsersHelper
-  def avatar_name(user)
-    if user.avatar_image_name.present?
-      user.avatar_image_name
-    else
-      "default.png"
-    end
-  end
+  
 end
 
